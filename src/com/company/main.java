88273f84a6cf088ccaa92f00a4576eb50566d3e5
Alexander Ladoshkin <alexander@Alexanders-MacBook-Air.local>
2022-02-14 22:38:06 +0200

    Initial commit

---
+++ src/com/company/main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.print("Введите два числа: ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            double a = 0, b = 0, result = 0;
+            try {
+                a = Integer.parseInt(reader.readLine());
+                b = Integer.parseInt(reader.readLine());
+            } catch (IOException e) {
+                System.out.print("Error: " + e.getMessage());
+            }
+            System.out.println("Введите знак операции: ");
+            BufferedReader raed = new BufferedReader(new InputStreamReader(System.in));
+            String sing = "";
+            try {
+                sing = raed.readLine();
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+            System.out.println("a: " + a + " b:  " + b + " sing: " + sing);
+            switch (sing) {
+                case "+": {
+                    result = a + b;
+                    break;
+                }
+                case "-": {
+                    result = a - b;
+                    break;
+                }
+                case "/": {
+                    result = a / b;
+                    break;
+                }
+                case "*": {
+                    result = a * b;
+                    break;
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
