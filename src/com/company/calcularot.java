faeafad3c7cd5d1b98714a25c28da17b2ce061bb
InnaLadoshkina <99286500+InnaLadoshkina@users.noreply.github.com>
2022-02-15 17:03:33 +0200

    Update calcularot.java

--- src/com/company/calcularot.java
+++ src/com/company/calcularot.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Calcularot {
+public class Calculator {
     public static void main(String[] args) {
         while (true) {
             System.out.print("Введите первое число: ");
